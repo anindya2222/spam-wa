ccd126d99665c8d6a39ce19154956f4f24b9d635
Anindya Prameswari <77677191+anindya2222@users.noreply.github.com>
2022-09-09 10:47:13 +0700

    Update spam.py

--- spam.py
+++ spam.py
@@ -6,7 +6,7 @@ piknik=('ayo transfer','ayo bayar')
 time.sleep(8)
 for i in range(20):
     a=random.choice(piknik)
-    pg.write("ndri" + a)
+    pg.write("nindy" + a)
     pg.press('enter')
     
     
